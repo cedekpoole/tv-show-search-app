72f862d8fb9e2396cc50e0c1dcf3f33cd9570bd7
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-02-14 22:09:12 +0000

    add card container which holds tv show images

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,22 @@
 const form = document.querySelector("#search-form");
+const cardRow = document.querySelector("#card-row");
 
 form.addEventListener('submit', async function(e) {
     e.preventDefault();
     const tvShow = this.elements.query.value;
-    const res = await axios.get(`https://api.tvmaze.com/singlesearch/shows?q=${tvShow}`);
+    const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${tvShow}`);
     console.log(res.data)
+    this.elements.query.value = "";
+    makeShowImage(res.data)
 })
+
+const makeShowImage = (shows) => {
+    for (let tvShow of shows){
+        const div = document.createElement("DIV");
+        div.setAttribute("class", "col-3 mb-2")
+        const image = document.createElement("IMG");
+        image.src = tvShow.show.image.medium;
+        div.append(image)
+        cardRow.append(div)
+    }
+}
