c474f34d994cb49317b341f07422a5b05f8d7b90
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-02-15 21:09:16 +0000

    add config variable and conditional statement

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,25 @@
 const form = document.querySelector("#search-form");
 const cardRow = document.querySelector("#card-row");
 
-form.addEventListener('submit', async function(e) {
-    e.preventDefault();
-    const tvShow = this.elements.query.value;
-    const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${tvShow}`);
-    console.log(res.data)
-    this.elements.query.value = "";
-    makeShowImage(res.data)
-})
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  cardRow.innerHTML = "";
+  const tvShow = this.elements.query.value;
+  const config = { params: { q: tvShow } };
+  const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+  this.elements.query.value = "";
+  makeShowImage(res.data);
+});
 
 const makeShowImage = (shows) => {
-    for (let tvShow of shows){
-        const div = document.createElement("DIV");
-        div.setAttribute("class", "col-3 mb-2")
-        const image = document.createElement("IMG");
-        image.src = tvShow.show.image.medium;
-        div.append(image)
-        cardRow.append(div)
+  for (let tvShow of shows) {
+    if (tvShow.show.image) {
+      const div = document.createElement("DIV");
+      div.setAttribute("class", "col-3 mb-2");
+      const image = document.createElement("IMG");
+      image.src = tvShow.show.image.medium;
+      div.append(image);
+      cardRow.append(div);
     }
-}
+  }
+};
